0a7e955e0f9f5999d6864d946db3a4bdfcb7c662
pratiksolanki09 <ps6535754@gmail.com>
2024-12-01 13:00:00 +0530

    Adding spring boot initializer project

---
+++ src/main/java/com/rbac/RbaCxApplication.java
@@ -0,0 +1,13 @@
+package com.rbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbaCxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbaCxApplication.class, args);
+	}
+
+}
